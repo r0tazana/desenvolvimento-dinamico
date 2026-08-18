ba4b5445afa9de141de164dfc168b040428258ca
RODRIGO NASCIMENTO <05492577565@unijorge.com.br>
2026-08-18 20:44:51 -0300

    adicionei arquivo componente

---
+++ src/app/pessoa/pessoa.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa',
+  imports: [],
+  templateUrl: './pessoa.html',
+  styleUrl: './pessoa.css',
+})
+export class Pessoa {}
